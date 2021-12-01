20dacfef0e4472d37bcb39c77ed1febf6fac32e0
haseebzahid1 <mmianhaseeb444@gmail.com>
2021-12-01 17:21:16 +0500

    yoga

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meditation_app/konstant/Constant.dart';
+import 'package:meditation_app/screens/search.dart';
+
+import 'bottomnavigator/navigatorBar.dart';
+import 'medition/madition_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,85 +18,148 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: "Cairo",
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: Theme.of(context).textTheme.apply(displayColor: Colors.black),
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
+      bottomNavigationBar: NavigatorBottomPage(),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              height: size.height * .45,
+              decoration: const BoxDecoration(
+                color: Color(0xFFF5CEB8),
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+                ),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  const Text(
+                    "Good Mornign \nShishir",
+                    style: TextStyle(fontSize: 32,fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    child: const Search(),
+                  ),//Search
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .90,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        const CardWidget(imgPath: "assets/icons/Hamburger.svg", imgTitle: "Diet Recommendation",),
+                        const CardWidget(imgPath: "assets/icons/Excrecises.svg", imgTitle: "Kegel Exercises",),
+                        GestureDetector(
+                            onTap:(){
+                              Navigator.push(context, MaterialPageRoute(builder: (context){
+                                return const MeditionScreen();
+                              }));
+                            },
+                            child: const CardWidget(imgPath: "assets/icons/Meditation.svg", imgTitle: "Meditation",)),
+                        Material(
+                          color: Colors.red,
+                          child: InkWell(
+                              onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context){
+                                  return const MeditionScreen();
+                                }));
+                              },
+                              child: const CardWidget(imgPath: "assets/icons/yoga.svg", imgTitle: "Yoga",)),
+                        ),
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+                      ],
+                    ),
+                  ),//Grid Add
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+
+
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({Key? key, required this.imgPath, required this.imgTitle}) : super(key: key);
+  final String imgPath;
+  final String imgTitle;
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: const [
+              BoxShadow(
+                offset:Offset(0,17),
+                blurRadius: 17,
+                spreadRadius: -23,
+                color: kShadowColor,
+              ),
+            ],
+          ),
+          child: Padding(
+            padding:const EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                SvgPicture.asset(imgPath),
+                const Spacer(),
+                Text(imgTitle,
+                  textAlign: TextAlign.center,
+                  style:TextStyle(fontSize: 15) ,
+                ),
+              ],
+            ),
+          ),
+        ),
+
     );
   }
 }
